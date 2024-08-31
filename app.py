4d64b9dd9ee43369a182bc00a2d8eafb818de6a3
ahae29 <82927971+ahae29@users.noreply.github.com>
2024-08-31 21:25:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 # 모델 로드
 model = load_model('my_model.h5')  # Teachable Machine에서 저장한 모델 파일 경로
+model.save('my_model.h5')
 
 # 클래스 이름
 class_names = ['가위', '바위']  # 클래스 이름을 실제 클래스에 맞게 수정하세요.
